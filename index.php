522a9c9bf38b4f94e813febf15dd9a4756af006d
onizzzuka <stalkersss@gmail.com>
2025-04-11 17:58:18 +0300

    remove unnecessary code

--- index.php
+++ index.php
@@ -6,13 +6,6 @@ use App\Parser\LockParser;
 use App\Parser\TomlParser;
 use Yosymfony\Toml\Toml;
 
-$parsed_items = [
-    TomlParser::getItems(Toml::parseFile(__DIR__ . "/files_for_parse/Cargo.toml")),
-    LockParser::getItems(Toml::parseFile(__DIR__ . "/files_for_parse/Cargo.lock")),
-    JsonParser::getItems(json_decode(file_get_contents(__DIR__ . "/files_for_parse/cargo-sources.json"), TRUE, 512, JSON_THROW_ON_ERROR)),
-    JsonParser::getItems(json_decode(file_get_contents(__DIR__ . "/files_for_parse/cargo-sources-gatherer.json"), TRUE, 512, JSON_THROW_ON_ERROR)),
-];
-
 $packages = array_merge(
     TomlParser::getItems(Toml::parseFile(__DIR__ . "/files_for_parse/Cargo.toml")),
     LockParser::getItems(Toml::parseFile(__DIR__ . "/files_for_parse/Cargo.lock")),
