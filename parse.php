b8970232701c6ae7ce7492e96cbe4c49da58e212
onizzzuka <stalkersss@gmail.com>
2025-11-17 02:17:45 +0200

    Remove unused require statement from parse.php

--- parse.php
+++ parse.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Parser;
 
-//require_once __DIR__ . '/src/Parser.php';
-
 new Parser(
     filesForParseDirName: __DIR__ . "/files_for_parse",
     outputDirName: __DIR__ . "/output",
